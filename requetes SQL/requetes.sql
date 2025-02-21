0b9af7b5e895bc33d1679d035a863a55cc309cf1
mansorgueye <63796674+mansorgueye@users.noreply.github.com>
2025-02-21 01:06:04 +0100

    Update requetes.sql

--- requetes SQL/requetes.sql
+++ requetes SQL/requetes.sql
@@ -1,21 +1,21 @@
-# 2.Première partie 
+# 2.Première partie (BigQuery)
 
 SELECT date, SUM(prod_qty*prod_price) as ventes
 FROM TRANSACTION 
-WHERE CONVERT(DATETIME, date, 3) BETWEEN "2019-01-01" AND "2019-12-31"
+WHERE date BETWEEN "2019-01-01" AND "2019-12-31"
 GROUP BY  date 
-ORDER BY date ASC;
+ORDER BY date;
 
 
 
 
-# 3. Seconde partie
+# 3. Seconde partie (BigQuery)
 
-SELECT client_id, 
-SUM( if ( product_type="MEUBLE", prod_qty*prod_price, 0 ) ) as ventes_meuble, 
-SUM( if ( product_type="DECO", prod_qty*prod_price, 0 ) ) as ventes_deco, 
+SELECT t.client_id as client_id, 
+SUM( if ( p.product_type="MEUBLE", t.prod_qty*t.prod_price, 0 ) ) as ventes_meuble, 
+SUM( if ( p.product_type="DECO", t.prod_qty*t.prod_price, 0 ) ) as ventes_deco, 
 FROM TRANSACTIONS t
 LEFT JOIN PRODUCT_NOMENCLATURE p
 ON t.prod_id=p.product_id
-WHERE CONVERT(DATETIME, date, 3) BETWEEN "2019-01-01" AND "2019-12-31"
-GROUP BY  client_id
+WHERE t.date BETWEEN "2019-01-01" AND "2019-12-31"
+GROUP BY  t.client_id
